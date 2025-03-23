e4a8b47e167082e5d68ad4e66693dd01277f3dda
nafisa <nafisatasnim8855@gmail.com>
2025-03-23 10:09:30 +0600

    Consolidate condistional expression in roundToNearestFive() from method calculateFlightTime() 	and simplify if-else conditions in Flight class
    Signed-off-by: nafisa <nafisatasnim8855@gmail.com>

--- AirlineReservationSystem/src/Flight.java
+++ AirlineReservationSystem/src/Flight.java
@@ -128,26 +128,19 @@ public class Flight extends FlightDistance {
         String[] timeArray = timeInString.replace('.', ':').split(":");
         int hours = Integer.parseInt(timeArray[0]);
         int minutes = Integer.parseInt(timeArray[1]);
-        int modulus = minutes % 5;
         // Changing flight time to make minutes near/divisible to 5.
-        if (modulus < 3) {
-            minutes -= modulus;
-        } else {
-            minutes += 5 - modulus;
-        }
+        minutes = roundToNearestFive(minutes);
+
         if (minutes >= 60) {
             minutes -= 60;
             hours++;
         }
-        if (hours <= 9 && Integer.toString(minutes).length() == 1) {
-            return String.format("0%s:%s0", hours, minutes);
-        } else if (hours <= 9 && Integer.toString(minutes).length() > 1) {
-            return String.format("0%s:%s", hours, minutes);
-        } else if (hours > 9 && Integer.toString(minutes).length() == 1) {
-            return String.format("%s:%s0", hours, minutes);
-        } else {
-            return String.format("%s:%s", hours, minutes);
-        }
+        return String.format("%02d:%02d", hours, minutes);
+    }
+
+    private int roundToNearestFive(int minutes) {
+        int modulus = minutes % 5;
+        return (modulus < 3) ? (minutes - modulus) : (minutes + (5 - modulus));
     }
 
     /**
